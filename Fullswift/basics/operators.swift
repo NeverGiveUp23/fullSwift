3a738e5386c0dcf094adcd04e1f2f4b3fb4ed2d4
felix <felixvjr0@gmail.com>
2025-02-19 09:51:52 -0500

    func, generics, half-open-range loops, one side ranges, closed range loops

--- Fullswift/basics/operators.swift
+++ Fullswift/basics/operators.swift
@@ -18,10 +18,48 @@ var userDefaultColorName: String?;
 var colorNameToUse = userDefaultColorName ?? defaultColorName;
 // userDefaultColorName is nil, so colorName is set to the default of 'red' -> if it wasnt nil, it wouldve been set to that color instead
 
-func loopControl(_ start: Int, _ end: Int) -> Int {
+func loopControl(_ start: Int, _ end: Int) {
     for index in start...end {
-        return "\(index) times 5 is \(index * 5)";
+        print( "\(index) times 5 is \(index * 5)");
     }
 }
 
+// for half open ranges are good for index based arrays. -> it includes the first value but not the last value -> useful to count up to but not including the length of the list
+// this counts from 0 to 3 ifi have 4 in the array, so 0 - 3, not including 4
+func halfRange(_ lists: [String]) {
+    let count: Int = lists.count;
+    for i in 0..<count {
+        print("Person \(i + 1) is called \(lists[i])");
+    }
+}
 
+// one side ranges -> a range that includes all the elements of an array from index 2 to the end of the array.
+
+func oneSideRange(range: [String]) -> [String]{
+    var namesIn: [String] = [];
+    
+    for name in range[2...] {
+        namesIn.append(name);
+    }
+    return namesIn;
+}
+
+// another on sided range
+func OneSide(range: [String]) -> [String] {
+    var begin: [String] = [];
+    
+    for name in range[...2] {
+        begin.append(name);
+    }
+    
+    return begin;
+}
+// generic one sided function to list arrays
+func genericOneSided<T>(_ range: [T]) -> [T] {
+    var change: [T] = [];
+    
+    for name in range[...2] {
+        change.append(name);
+    }
+    return change;
+}
--- Fullswift/main.swift
+++ Fullswift/main.swift
@@ -9,8 +9,28 @@ import Foundation
 
 print("Hello, World!");
 
-print(loopControl(1, 5))
+loopControl(1, 5);
+
+print("-------------------------");
+
+let names: [String] = ["Felix", "Mike", "Ashley", "Amanda"];
+halfRange(names);
 
 
+print("-------------------------");
 
+// going from index 2 and on so -> [2...]
+print(oneSideRange(range: names));
+// Ashley
+// Amanda
+
+print(OneSide(range: names))
+// ["Felix", "Mike", "Ashley"] -> [...2] -> going from beginning of the array to the 2nd index -> 0, 1, 2.
+
+let numberArrays: [Int] = [1, 33, 53, 2];
+let doubleNum: [Double] = [22.3, 66.9, 55.2, 11.8, 19.4];
+print(genericOneSided(doubleNum));
+// [1, 33, 53] -> ["Felix", "Mike", "Ashley"]
+// [22.3, 66.9, 55.2]
+// due to generics, it works with all data types if needed
 
